3b6bcdb4f6ab3d2d8bd89926f47a03c220c8ed27
Kevin Rousseeuw <k.rousseeuw@hotmail.com>
2019-12-02 22:28:53 +0100

    Attempted to make a connection but it wont work, I cannot connect server to client through JS

--- src/SecureSessionPeer.js
+++ src/SecureSessionPeer.js
@@ -1,56 +1,38 @@
 const _sodium = require('libsodium-wrappers');
 const Decryptor = require('./Decryptor');
 const Encryptor = require('./Encryptor');
-const EventEmitter = require('events')
 
-
-module.exports = async (peer) => {
+// Server
+module.exports = async(peer) => {
     await _sodium.ready;
-    
-    var keypair = _sodium.crypto_kx_keypair();
-    
+
+    var keypair = _sodium.crypto_kx_keypair();       
+
     const publicKey = keypair.publicKey;
-    const secretKey = keypair.privateKey;    
+    const privateKey = keypair.privateKey;    
+    var encryptor;
+    var decryptor;
 
-    sharedKey = _sodium.crypto_kx_server_session_keys(publicKey, secretKey, peer.publicKey);
-
-    rx = sharedKey.sharedRx;
-    tx = sharedKey.sharedTx;    
-
-    decryptor = await Decryptor(publicKey);
-    encryptor = await Encryptor(publicKey);
-
-    res = _sodium.crypto_secretstream_xchacha20poly1305_init_push(publicKey);
-    [state_out, header] = [res.state, res.header];
-
-    otherPeer = {
-        signature: null
-    };
-   
+    if(peer){    
+        clientKeys = _sodium.crypto_kx_client_session_keys(publicKey, privateKey, peer.publicKey);
+        decryptor = await Decryptor(clientKeys.sharedRx);
+        encryptor = await Encryptor(clientKeys.sharedTx);
+        
+    } else {
+        serverKeys = _sodium.crypto_kx_server_session_keys(publicKey, privateKey);
+        decryptor = await Decryptor(serverKeys.sharedRx);
+        encryptor = await Encryptor(privateKey.sharedTx);        
+    } 
+    
+    
     return Object.freeze({
         publicKey: publicKey,
         encrypt: (msg) => {
             return encryptor.encrypt(msg);
         },
-        decrypt: (ciphertext, nonce) => {
+        decrypt: (ciphertext, nonce) => {            
             return decryptor.decrypt(ciphertext, nonce);
         },
-        send: (msg) => {
-            _sodium.crypto_secretstream_xchacha20poly1305_push(state_out,
-                _sodium.from_string(msg), null,
-                _sodium.crypto_secretstream_xchacha20poly1305_TAG_MESSAGE);
-        },
-        receive: () => {
-            let state_in = _sodium.crypto_secretstream_xchacha20poly1305_init_pull(header, publicKey);
-            let r1 = _sodium.crypto_secretstream_xchacha20poly1305_pull(state_in);
-            let [m1, tag1] = [_sodium.to_string(r1.message), r1.tag];
 
-            return m1;
-        },
-        getCipher: (msg) => {
-            _sodium.crypto_secretstream_xchacha20poly1305_push(state_out,
-                _sodium.from_string(msg), null,
-                _sodium.crypto_secretstream_xchacha20poly1305_TAG_MESSAGE);
-        }        
     });
-};
+}
